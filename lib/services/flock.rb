2f60a02b9f619808ce0d7639b925f2d136fe29ff
Ayush Jindal <ayush.j@directi.com>
2016-05-02 12:32:43 +0530

    Update Flock service hook to support Flock app store

--- lib/services/flock.rb
+++ lib/services/flock.rb
@@ -1,12 +1,19 @@
 class Service::Flock < Service::Base
   title 'Flock'
 
-  string :url, :placeholder => 'Incoming Webhook URL',
-                 :label => 'Flock Incoming Webhook URL. <br />' \
-                   'To create an incoming webhook, go to your Flock admin panel and switch to "Webhooks" tab'
+  string :url, :placeholder => 'Flock Webhook URL',
+                 :label => 'Go to the <a href="https://apps.flock.co/crashlytics">Flock App Store</a> and install the Crashlytics app. <br />' \
+                    'Generate the Flock Webhook URL and paste it below:'
 
   def receive_verification
-    response = post_to_flock('Successfully configured Flock service hook with Crashlytics')
+    if app_store_flock_url?
+      response = post_to_flock(:event => 'verification', :payload_type => 'none')
+    elsif api_flock_url?
+      response = post_to_flock(:text => 'Successfully configured Flock service hook with Crashlytics')
+    else
+      display_error("#{self.class.title} verification failed - URL is not an expected format.")
+    end
+
     if response.success?
       log('verification successful')
     else
@@ -15,8 +22,14 @@ class Service::Flock < Service::Base
   end
 
   def receive_issue_impact_change(payload)
-    message = extract_flock_message(payload)
-    response = post_to_flock(message)
+    if app_store_flock_url?
+      response = post_to_flock(payload)
+    elsif api_flock_url?
+      response = post_to_flock(:text => extract_flock_message(payload))
+    else
+      display_error("#{self.class.title} issue impact change failed - URL is not an expected format.")
+    end
+
     if response.success?
       log('issue_impact_change successful')
     else
@@ -32,12 +45,18 @@ class Service::Flock < Service::Base
     "More information: #{payload[:url]}"
   end
 
-  def post_to_flock(message)
-    body = { :text => message }
+  def app_store_flock_url?
+    config[:url].start_with?('https://apps.flock')
+  end
 
+  def api_flock_url?
+    config[:url].start_with?('https://api.flock')
+  end
+
+  def post_to_flock(payload)
     http_post(config[:url]) do |request|
       request.headers['Content-Type'] = 'application/json'
-      request.body = body.to_json
+      request.body = payload.to_json
     end
   end
 end
